593ee7f5c77c677bf9db06fb00ca7a7db8e35834
GrittyFrog <grittyfrog@fastmail.com>
2023-10-22 00:29:16 +1100

    Remove most commands but the main one
    We're not really command based anymore, the only functionality that we
    couldn't do in the UI was reloading the current config. (Now there's a button)

--- MacroMate/Commands/PluginCommandManager.cs
+++ MacroMate/Commands/PluginCommandManager.cs
@@ -9,58 +9,18 @@ namespace MacroMate.Commands;
  * Manages the commands introduced by this plugin
  */
 public class PluginCommandManager : IDisposable {
-    private const string CommandName = "/macromate";
-
-    public readonly IReadOnlyDictionary<string, ICommand> Subcommands = new List<ICommand> {
-        new ConfigCommand(),
-        new ConditionsCommand(),
-        new HelpCommand(),
-        new ReloadCommand()
-    }.ToDictionary(command => command.CommandName);
-
     public PluginCommandManager() {
-        // These `AddHandler` calls don't cause anything to happen when the command is executed.
-        // This is because Dalamud can't dispatch commands like "/macromate help" or any command with a space in it.
-        // We only make these calls to make the command appear in the "help" section of the plugin.
-        foreach(var command in Subcommands.Values) {
-            Env.CommandManager.AddHandler(FullCommandName(command), new CommandInfo((_, _) => {}) {
-                HelpMessage = command.HelpMessage
-            });
-        }
-
-        // This `AddHandler` call actually does all the work.
-        Env.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand) {});
+        var mainCommandInfo = new CommandInfo(OnMainCommand) { HelpMessage = "Show the Macro Mate window" };
+        Env.CommandManager.AddHandler("/macromate", mainCommandInfo);
+        Env.CommandManager.AddHandler("/mm", mainCommandInfo);
     }
 
-    public void RunCommand(string command, List<string>? arguments = null) {
-        arguments ??= new List<string>();
-        Subcommands[command].Execute(arguments);
-    }
-
-    private void OnCommand(string command, string arguments) {
-        var splitArgs = arguments.Split(" ").Where(arg => arg != "").ToList();
-        if (splitArgs.Count == 0) {
-            Subcommands["config"].Execute(new());
-            return;
-        }
-
-        var subcommandName = splitArgs[0];
-        var subcommand = Subcommands.GetValueOrDefault(subcommandName);
-        if (subcommand != null) {
-            var remainingArgs = splitArgs.Skip(1).ToList();
-            subcommand.Execute(remainingArgs);
-        } else {
-            Env.ChatGui.PrintError($"Unrecognised subcommand: '{subcommandName}'");
-            Subcommands["help"].Execute(new());
-        }
+    private void OnMainCommand(string command, string arguments) {
+        Env.PluginWindowManager.MainWindow.Toggle();
     }
 
     public void Dispose() {
-        foreach(var command in Subcommands.Values) {
-            Env.CommandManager.RemoveHandler(FullCommandName(command));
-        }
-        Env.CommandManager.RemoveHandler(CommandName);
+        Env.CommandManager.RemoveHandler("/macromate");
+        Env.CommandManager.RemoveHandler("/mm");
     }
-
-    private string FullCommandName(ICommand command) => $"{CommandName} {command.CommandName}";
 }
