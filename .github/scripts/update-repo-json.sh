972aa7c691664af0f763631483fcb780365fe11b
GrittyFrog <grittyfrog@fastmail.com>
2023-11-19 10:50:44 +1100

    Fix icon url in repo.json

--- .github/scripts/update-repo-json.sh
+++ .github/scripts/update-repo-json.sh
@@ -4,12 +4,14 @@ set -euo pipefail
 BUILD_JSON=MacroMate/bin/x64/Release/MacroMate/MacroMate.json
 BUILD_VERSION=$(jq -r .AssemblyVersion $BUILD_JSON)
 
+ICON_URL="https://raw.githubusercontent.com/grittyfrog/MacroMate/v${BUILD_VERSION}/MacroMate/Images/icon.png"
 VERSION_URL="https://github.com/grittyfrog/MacroMate/releases/download/v${BUILD_VERSION}/MacroMate.zip"
 
 UPDATED_JSON=$(jq -n '$ARGS.named' \
     --arg AssemblyVersion "${BUILD_VERSION}" \
     --arg Description "$(jq -r .Description $BUILD_JSON)" \
     --arg Punchline "$(jq -r .Punchline $BUILD_JSON)" \
+    --arg IconUrl "${ICON_URL}" \
     --arg DownloadLinkInstall "${VERSION_URL}" \
     --arg DownloadLinkTesting "${VERSION_URL}" \
     --arg DownloadLinkUpdate "${VERSION_URL}"
