64f38774bf173a6ef2b08d12066d83c112743c0f
GrittyFrog <grittyfrog@fastmail.com>
2023-11-27 17:44:14 +1100

    Fix PvP Condition check

--- MacroMate/Conditions/PvpStateCondition.cs
+++ MacroMate/Conditions/PvpStateCondition.cs
@@ -23,7 +23,17 @@ public record class PvpStateCondition(PvpStateCondition.State state) : IConditio
 
     public ICondition.IFactory FactoryRef => throw new System.NotImplementedException();
 
-    public bool SatisfiedBy(ICondition other) => this.Equals(other);
+    public bool SatisfiedBy(ICondition other) {
+        var otherPvpState = other as PvpStateCondition;
+        if (otherPvpState == null) { return false; }
+
+        // IN_PVP_NO_WOLVES_DEN satisfies both itself and IN_PVP.
+        if (this.state == State.IN_PVP) {
+            return otherPvpState.state == State.IN_PVP || otherPvpState.state == State.IN_PVP_NO_WOLVES_DEN;
+        }
+
+        return this.state == otherPvpState.state;
+    }
 
     public static PvpStateCondition Current() {
         if (Env.ClientState.IsPvPExcludingDen) { return new PvpStateCondition(State.IN_PVP_NO_WOLVES_DEN); }
