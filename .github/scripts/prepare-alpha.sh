d121c2299694b482165e8c1dddd3bf95ba6f0f45
GrittyFrog <grittyfrog@fastmail.com>
2024-07-03 16:11:23 +1000

    Version 1.0.12.0
    - 7.0 support

--- .github/scripts/prepare-alpha.sh
+++ .github/scripts/prepare-alpha.sh
@@ -9,7 +9,6 @@ cp -r ${BUILD_LOCATION} ${ALPHA_LOCATION}
 mv ${ALPHA_LOCATION}/MacroMate.deps.json ${ALPHA_LOCATION}/MacroMateAlpha.deps.json
 mv ${ALPHA_LOCATION}/MacroMate.dll ${ALPHA_LOCATION}/MacroMateAlpha.dll
 mv ${ALPHA_LOCATION}/MacroMate.json ${ALPHA_LOCATION}/MacroMateAlpha.json
-mv ${ALPHA_LOCATION}/MacroMate.pdb ${ALPHA_LOCATION}/MacroMateAlpha.pdb
 
 # Reconstruct the release zip
 rm -rf "${ALPHA_LOCATION}/MacroMate"
