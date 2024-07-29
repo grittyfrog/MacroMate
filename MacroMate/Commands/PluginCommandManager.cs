a1b22456479915ba80570a4adc2c6d4fafe6a627
GrittyFrog <grittyfrog@fastmail.com>
2024-07-30 01:06:13 +1000

    Add `/mm open` and `/mm help` subcommands

--- MacroMate/Commands/PluginCommandManager.cs
+++ MacroMate/Commands/PluginCommandManager.cs
@@ -1,5 +1,7 @@
 using System;
 using Dalamud.Game.Command;
+using Dalamud.Game.Text.SeStringHandling;
+using MacroMate.MacroTree;
 
 namespace MacroMate.Commands;
 
@@ -14,7 +16,57 @@ public class PluginCommandManager : IDisposable {
     }
 
     private void OnMainCommand(string command, string arguments) {
-        Env.PluginWindowManager.MainWindow.Toggle();
+        if (arguments.Trim() == "") {
+            Env.PluginWindowManager.MainWindow.Toggle();
+            return;
+        }
+
+        var subcommandIndex = arguments.IndexOf(" ");
+        var subcommand = subcommandIndex == -1 ? arguments : arguments.Substring(0, subcommandIndex).Trim();
+        var subarguments = subcommandIndex == -1 ? null : arguments.Substring(subcommandIndex).Trim();
+        switch (subcommand) {
+            case "open": OnOpenSubcommand(command, subarguments); break;
+            case "help": PrintHelp(); return;
+            default:
+                Env.ChatGui.PrintError($"unrecognized subcommand: '{subcommand}'");
+                return;
+        }
+    }
+
+    private void PrintHelp() {
+        var seStr = new SeStringBuilder()
+            .Append("[MacroMate Help]\n")
+            .Append("Usage: '/macromate' or '/mm' - toggle the config window.\n")
+            .Append("Usage: '/macromate <command>' or '/mm command' - run a subcommand.\n")
+            .Append("\n")
+            .Append("Subcommands:\n")
+            .Append("  open <path> - opens the macro identified by path (see Help for path details)\n")
+            .Append("  help - print this message")
+            .Build();
+
+        Env.ChatGui.Print(seStr);
+    }
+
+    /// Syntax: `/mm open <path>`. Assumes `arguments` is the full path (to allow for spaces)
+    private void OnOpenSubcommand(string command, string? path) {
+        if (path == null || path == "") {
+            Env.ChatGui.PrintError($"Expected {command} open <path> (try {command} help)");
+            return;
+        }
+
+        try {
+            var parsedPath = MacroPath.ParseText(path);
+            var target = Env.MacroConfig.Root.Walk(parsedPath);
+            if (target == null) { Env.ChatGui.PrintError($"no node found at '{path}'"); return; }
+            if (target is MateNode.Macro macroTarget) {
+                Env.PluginWindowManager.MacroWindow.ShowOrFocus(macroTarget);
+            } else {
+                 Env.ChatGui.PrintError($"expected macro at '{path}'");
+                 return;
+            }
+        } catch (ArgumentException ex) {
+            Env.ChatGui.PrintError(ex.Message);
+        }
     }
 
     public void Dispose() {
