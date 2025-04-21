c930f16da97e2759d5bf6177b6038104ca80e18d
GrittyFrog <grittyfrog@fastmail.com>
2025-04-21 15:15:34 +1000

    Fix issue when loading obsolete PlayerConditions

--- MacroMate/Serialization/V1/MacroMateSerializerV1.cs
+++ MacroMate/Serialization/V1/MacroMateSerializerV1.cs
@@ -1,14 +1,20 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Xml.Serialization;
+using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Utility;
 using MacroMate.MacroTree;
 
 namespace MacroMate.Serialization.V1;
 
 public static class MacroMateSerializerV1 {
-    private static XmlSerializer configXmlSerializer = new XmlSerializer(typeof(MacroMateV1XML));
-    private static XmlSerializer mateNodeXmlSerializer = new XmlSerializer(typeof(MateNodeXML));
+    private static XmlAttributeOverrides xmlAttributeOverrides = new XmlAttributeOverrides()
+        .AllowObsoleteAttributeSerialization();
+
+    private static XmlSerializer configXmlSerializer = new XmlSerializer(typeof(MacroMateV1XML), xmlAttributeOverrides);
+    private static XmlSerializer mateNodeXmlSerializer = new XmlSerializer(typeof(MateNodeXML), xmlAttributeOverrides);
 
     public static string Export(MateNode tree) {
         var xml = MateNodeXML.From(tree);
@@ -58,5 +64,26 @@ public static class MacroMateSerializerV1 {
                 throw;
             }
         }
+    }
+
+    /// <summary>
+    /// By default XmlSerializer will refuse to serialize fields/values marked [Obsolete]. This causes issues
+    /// when upgrading Dalamud since our config may have an "old" name that will fail to serialize and cause
+    /// Macro Mate not to load.
+    /// </summary>
+    private static XmlAttributeOverrides AllowObsoleteAttributeSerialization(this XmlAttributeOverrides self) {
+        var enumClasses = new[] { typeof(ConditionFlag) };
+        var dontIgnore = new XmlAttributes { XmlIgnore = false };
+        foreach (var enumClass in enumClasses) {
+            var obsoleteValues = enumClass
+              .GetFields(BindingFlags.Public | BindingFlags.Static)
+              .Where(fieldInfo => fieldInfo.GetCustomAttribute(typeof (ObsoleteAttribute)) != null);
+
+            foreach (var obsoleteValue in obsoleteValues) {
+                self.Add(enumClass, obsoleteValue.Name, dontIgnore);
+            }
+        }
+
+        return self;
     }
 }
