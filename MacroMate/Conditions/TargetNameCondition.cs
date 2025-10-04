652ad463a327d609d5a62b34df9852a0b2cd47f3
grittyfrog <grittyfrog@fastmail.com>
2025-10-05 00:05:06 +1000

    Fix flake pin

--- MacroMate/Conditions/TargetNameCondition.cs
+++ MacroMate/Conditions/TargetNameCondition.cs
@@ -107,7 +107,7 @@ public record class TargetNameCondition(
         }
 
         // For everything else we assume their usual Data sheet has their name.
-        return new TargetNameCondition(target.ObjectKind, target.BaseId);
+        return new TargetNameCondition(target.ObjectKind, target.DataId);
     }
 
     public string Name => TargetKind switch {
